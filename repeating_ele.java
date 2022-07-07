fb4429e56e9d909ab5b06ce27230d134197ef868
TUSHAR PRAJAPATI <72430130+bobbyprajapati96@users.noreply.github.com>
2022-07-07 20:39:21 +0530

    Add files via upload

---
+++ repeating_ele.java
@@ -0,0 +1,50 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class repeating_ele {
+    public static void main(String[] args) {
+        int arr[]={10, 30, 40, 20, 10, 20, 50,50,10};
+//        boolean visited[]=new boolean[arr.length];
+//        Arrays.fill(visited,false);
+//        for(int i=0;i<arr.length;i++)
+//        {
+//            if(visited[i]==true)continue;
+//            int count=1;
+//            for(int j=i+1;j<arr.length;j++)
+//            {
+//                if(arr[i]==arr[j])
+//                {
+//                    visited[j]=true;
+//                    count++;
+//                }
+//            }
+//            if(count!=1)
+//                System.out.print(arr[i]+" ");
+//        }
+
+        //method 2 using hashmap
+        Map<Integer, Integer> mp = new HashMap<>();
+
+// Traverse through array elements and
+// count frequencies
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (mp.containsKey(arr[i]))
+            {
+                mp.put(arr[i], mp.get(arr[i]) + 1);
+            }
+            else
+            {
+                mp.put(arr[i], 1);
+            }
+        }
+// Traverse through map and print frequencies
+        for (Map.Entry<Integer, Integer> entry : mp.entrySet())
+        { if(entry.getValue()>1)
+            System.out.println(entry.getKey()+" ");
+        }
+    }
+}
