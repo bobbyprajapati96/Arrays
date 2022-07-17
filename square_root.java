877ffa91e6e04e942b2d34289d06125db2b69e7b
TUSHAR PRAJAPATI <72430130+bobbyprajapati96@users.noreply.github.com>
2022-07-18 00:23:40 +0530

    Update square_root.java

--- square_root.java
+++ square_root.java
@@ -17,7 +17,30 @@ public class square_root {
             }
         }
         return ans;
+        
+        //leetcode soln
+//     long res = -1;
+//     long low = 0;
+//     long high = x;
+    
+//     //square root of a number lies between 0 and the number itself
+//     // And since 0 to number is sorted so we can apply binary search!
+  
+//     //since mid*mid can exceed int limit so I used long as datatype
+    
+//     while(low<=high){
+//         long mid = low+(high-low)/2; 
+//         if(mid*mid==x) return (int) mid;  
+//         else if(x>(mid*mid)){
+//             res = mid;
+//             low=mid+1;
+//         } else{
+//             high = mid-1;
+//         }
+//     }
+//     return (int)res;
     }
+    
 
     public static void main(String[] args) {
          int n=49;
